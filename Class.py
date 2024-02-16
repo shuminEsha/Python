b25c19c282997097f94f94118adbff5140544bb9
Python <shuminnahar8di@gmail.com>
2024-02-16 20:54:55 +0600

    update class

--- Class.py
+++ Class.py
@@ -1,1 +1,14 @@
-print("Esha")
+class balance:
+    def __init__(self,account_balance):
+        self.balance=account_balance
+        self.min_withdraw=100
+        self.max_withdraw=100000
+
+
+    def get_balance(self):
+        return self.balance
+    
+    def deposite(self,amount):
+        if amount >0 :
+            self.balance+=amount
+            
